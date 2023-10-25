e740e499c6ff030dd1a659db54d0d4c30143a455
Michelotto <vitor.silva10@estudante.ifms.edu.br>
2023-10-25 10:14:11 -0400

    correção

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
 import express from 'express';
 
+// Cria uma instância do aplicativo Express
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Olá, mundo!');
+type Filme = {
+    id: number,
+    titulo: string,
+    descricao: string,
+    foto: string,
+}
+const filmes_repositorio:Filme[] = []
+
+
+// Define uma rota padrão
+app.get('/filmes/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const filme = filmes_repositorio.find(filme => filme.id === id)
+    if (filme) {
+        res.send(filme)
+    } else {
+        res.status(404).send()
+    }
 });
 
 app.post('/filmes', (req, res) => {
-
-    const filme = {
-        titulo: 'Vingadores',
-        descricao: 'Filme dos Vingadores',
-        foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg',
-    }
+    const filme = req.body
+    filmes_repositorio.push(filme)
     res.status(201).send(filme)
-
 });
 
+
+// Inicia o servidor
 app.listen(3000, () => {
     console.log('Servidor iniciado na porta 3000');
 });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,5 @@
 import {describe, it, expect} from 'vitest'
 import axios from 'axios'
-
 describe('Primeiro teste da aplicação', () => {
   it('should pass', () => {
     expect(true).toBe(true)
@@ -10,18 +9,19 @@ describe('Primeiro teste da aplicação', () => {
 describe('Cadastro Filme', () => {
     it('Deve cadastrar um filme com sucesso', async () => {
         const filme = {
+            id: 1,
             titulo: 'Vingadores',
             descricao: 'Filme dos Vingadores',
             foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg',
         }
         const resposta = 
             await axios.post('http://localhost:3000/filmes', filme)
-        
         expect(resposta.status).toBe(201)
         expect(resposta.data).toEqual(filme)
-    }),
+    })
     it('deve listar um filme cadastrado', async () => {
       const filme = {
+          id: 1,
           titulo: 'Vingadores',
           descricao: 'Filme dos Vingadores',
           foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg',
@@ -29,14 +29,28 @@ describe('Cadastro Filme', () => {
       const resposta = await axios.post('http://localhost:3000/filmes', filme)
 
       //1) Buscar o filme cadastrado
-      const listaDeFilmes = await axios.get('http://localhost:3000/filmes')
-      const filmeCadastrado = listaDeFilmes.data;
-
+      const listaFilmes = await axios.get('http://localhost:3000/filmes/1')
+      const filmeCadastrado = listaFilmes.data
       //2)Verificar se o filme devolvido é igual ao que nós cadastramos
-      expect(filmeCadastrado[0]).toEqual(filme);
-    
+      expect(filmeCadastrado[0]).toEqual(filme)
       //3)Verificar se o tamanho da lista é igual a 1
-      expect(filmeCadastrado.length).toBe(1);
+      expect(filmeCadastrado.length).toBe(1)
+  })
+  it('deve listar outro filme cadastrado', async () => {
+    const filme = {
+        id: 2,
+        titulo: 'Barbie',
+        descricao: 'Filme da barbie',
+        foto: 'https://shop.mattel.com/cdn/shop/products/qgjefzecvqfdo6yesinu_e3dcff46-604e-4448-952a-c8a5755eeefc.jpg?v=1690236698',
+    }
+    const resposta = await axios.post('http://localhost:3000/filmes', filme)
 
+    //1) Buscar o filme cadastrado
+    const listaFilmes = await axios.get('http://localhost:3000/filmes/2')
+    const filmeCadastrado = listaFilmes.data
+    //2)Verificar se o filme devolvido é igual ao que nós cadastramos
+    expect(filmeCadastrado[0]).toEqual(filme)
+    //3)Verificar se o tamanho da lista é igual a 1
+    expect(filmeCadastrado.length).toBe(1)
   })
 })
