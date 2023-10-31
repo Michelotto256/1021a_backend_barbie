08dcce1d5ca77c2ca95ec0e5bab98cb0865831ca
Michelotto <vitor.silva10@estudante.ifms.edu.br>
2023-10-31 10:15:34 -0400

    correção

---
+++ src/aplicacao/salva-filme.use-case.test.ts
@@ -0,0 +1,17 @@
+import {describe, test, expect} from 'vitest'
+import SalvaFilme from './salva-filme.use-case'
+
+describe("Testando usecase de salvar filme",()=>{
+    test("Deve salvar um filme",()=>{
+        const filme = {
+            id: 1,
+            titulo:"test",
+            descricao:"test",
+            foto: "test"
+        }
+        const salvaFilme = new SalvaFilme()
+        const resultado = salvaFilme.execute(filme)
+
+        expect(resultado).toEqual(filme)
+    })
+})
---
+++ src/infra/banco/banco-em-memoria.test.ts
@@ -0,0 +1,18 @@
+import {describe,test,expect} from 'vitest'
+import BancoEmMemoria from './banco-em-memoria'
+
+describe("Banco em memória",()=>{
+    test("Deve salvar no banco em memoria",()=>{
+        const input = {
+            id: 1,
+            titulo:"test",
+            descricao:"test",
+            foto: "test",
+        }
+        const bancoEmMemoria = new BancoEmMemoria();
+        const result = bancoEmMemoria.salvar(input)
+        expect(result).toBe(true)
+        expect(bancoEmMemoria.dados).toHaveLength(1)
+        expect(bancoEmMemoria.dados).toEqual([input])
+    })
+})
