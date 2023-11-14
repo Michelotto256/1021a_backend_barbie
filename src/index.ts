2300f3f0be7940886f9d0eed3461de4f2b177680
Michelotto <vitor.silva10@estudante.ifms.edu.br>
2023-11-14 10:18:02 -0400

    rotas

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,8 @@ let filmes_repositorio:Filme[] = []
 
 
 // Define uma rota padrão
-app.get('/filmes/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const filme = filmes_repositorio.find(filme => filme.id === id)
-    if (!filme) res.status(404).send()
-    res.send(filme)        
+app.get('/filmes', (req, res) => {
+    res.send("filme")        
 });
 
 app.post('/filmes', (req:Request, res) => {
